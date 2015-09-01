6ce4d7dfbf757b6e41a6736cf76ba60e9dab58d6
Alejandro Casado Quijada <alex22alex33@gmail.com>
2015-09-01 14:09:04 +0000

    Prueba c9

--- bot.py
+++ bot.py
@@ -1,21 +1,100 @@
-# -*- coding: utf-8 -*-
+import telebot 
+from telebot import types 
+import time 
+import urllib2, cookielib, os.path
 
-import telebot # Librería de la API del bot.
-from telebot import types # Tipos para la API del bot.
-import time # Librería para hacer que el programa que controla el bot no se acabe.
 
-TOKEN = '132819610:AAHEdNGUT2PC8_oK44_LsdBRRvgYgDQ4NBo' # Nuestro tokken del bot (el que @BotFather nos dió).
+TOKEN = '130984576:AAGwSFR-CQcl0S0Bnc7oAyfGoD9Ko92Klhg'
 
 bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
 
-def listener(messages): # Con esto, estamos definiendo una función llamada 'listener', que recibe como parámetro un dato llamado 'messages'.
-    for m in messages: # Por cada dato 'm' en el dato 'messages'
-        cid = m.chat.id # Almacenaremos el ID de la conversación.
-        print "[" + str(cid) + "]: " + m.text # Y haremos que imprima algo parecido a esto -> [52033876]: /start
+def listener(messages): 
+    for m in messages: 
+        cid = m.chat.id 
+        print "[" + str(cid) + "]: " + m.text 
 
-bot.set_update_listener(listener) # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba.
+bot.set_update_listener(listener) 
 
-bot.polling(none_stop=True) # Con esto, le decimos al bot que siga funcionando incluso si encuentra algún fallo.
+bot.polling(none_stop=True) 
 
-while True: # Ahora le decimos al programa que no se cierre haciendo un bucle que siempre se ejecutará.
-    time.sleep(300) # Hacemos que duerma durante un periodo largo de tiempo para que la CPU no esté trabajando innecesáremente.
+#Horario grado ingenieria informatica
+
+@bot.message_handler(commands=['horario_gii'])
+def obtener_horario_gii(m):
+    cid = m.chat.id 
+    
+    if os.path.isfile('horario_gii.pdf'):
+        bot.send_document(cid, open( 'horario_gii.pdf', 'rb'))
+
+    else:
+        url = 'http://etsiit.ugr.es/pages/calendario_academico/horarios1516/horariosgii1516/!'
+        
+        cj = cookielib.CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        request = urllib2.Request(url)
+        
+        f = opener.open(request)
+        data = f.read()
+        f.close()
+        opener.close()
+        
+        FILE = open('horario_gii.pdf', "wb+")
+        FILE.write(data)
+        FILE.close()
+         
+        bot.send_document(cid, open( 'horario_gii.pdf', 'rb'))
+    #Horario grado ingenieria informatica
+
+
+@bot.message_handler(commands=['horario_git'])
+def obtener_horario_git(m):
+    cid = m.chat.id 
+    
+    if os.path.isfile('horario_git.pdf'):
+        bot.send_document(cid, open( 'horario_git.pdf', 'rb'))
+
+    else:
+        url = 'http://etsiit.ugr.es/pages/calendario_academico/horarios1516/horariosgitt1516/!'
+        
+        cj = cookielib.CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        request = urllib2.Request(url)
+        
+        f = opener.open(request)
+        data = f.read()
+        f.close()
+        opener.close()
+        
+        FILE = open('horario_git.pdf', "wb+")
+        FILE.write(data)
+        FILE.close()
+         
+        bot.send_document(cid, open( 'horario_git.pdf', 'rb'))
+        
+        
+@bot.message_handler(commands=['horario_gim'])
+def obtener_horario_gim(m):
+    cid = m.chat.id 
+    
+    if os.path.isfile('horario_gim.pdf'):
+        bot.send_document(cid, open( 'horario_gim.pdf', 'rb'))
+
+    else:
+        url = 'http://etsiit.ugr.es/pages/calendario_academico/horarios1516/horariosgim1516/!'
+        
+        cj = cookielib.CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        request = urllib2.Request(url)
+        
+        f = opener.open(request)
+        data = f.read()
+        f.close()
+        opener.close()
+        
+        FILE = open('horario_gim.pdf', "wb+")
+        FILE.write(data)
+        FILE.close()
+         
+        bot.send_document(cid, open( 'horario_gim.pdf', 'rb'))
+while True: 
+    time.sleep(0)
