34e92b8b0eb437c1f6a4fa7dcb0d001fcaae4585
Alejandro Casado Quijada <alex22alex33@gmail.com>
2015-09-02 17:10:46 +0000

    Update

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telebot import types
 import time 
 import urllib2, cookielib, os.path
 
-TOKEN = <Mi_token>
+TOKEN = '130984576:AAFDhHdC8kalZzbktH-wZMLp0txRYHyyvio'
 
 
 bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
@@ -15,79 +15,96 @@ def listener(messages):
 
 bot.set_update_listener(listener) 
 
+#Registro de actividad
+def log(m):
+        id_usuario = m.from_user.id
+        nombre_usuario = m.from_user.first_name
+        apellido_usuario = m.from_user.last_name
+        hora = time.strftime("%H:%M:%S")
+        fecha = time.strftime("%d/%m/%y")
+        
+        informacion = "["+ str(fecha) + ' ' + str(hora) + ' ' +str(nombre_usuario) + ' '  + str(apellido_usuario) + ' ' + str(id_usuario) + "]: " + m.text 
+        
+        aux = open( 'registro.txt', 'a') # Y lo insertamos en el fichero 'usuarios.txt'
+        aux.write( str(informacion) + "\n")
+        aux.close()
+        
 bot.polling(none_stop=True) 
 # Comprobamos si existe el fichero y se actua en consecuencia
-def comprobar_fichero(grado,id_chat):
+def comprobar_fichero(grado,m):
     
-    nombre_fichero = 'horarios_gi'
-    descriptor = 'horariosgi'
-    
+    try:
+        log(m)
+        nombre_fichero = 'horarios_gi'
+        descriptor = 'horariosgi'
+        
+        if grado == 'informatica':
+            descriptor += 'i1516/!'
+            nombre_fichero += 'i.pdf'
+            
+        elif grado == 'teleco':
+            descriptor += 'tt1516/!'
+            nombre_fichero += 't.pdf'
+        
+        elif grado == 'matematicas':
+           descriptor +=  'm1516/!'
+           nombre_fichero += 'm.pdf'
+        
+        if os.path.isfile(nombre_fichero):
+            bot.send_document(m.chat.id, open( nombre_fichero, 'rb'))
+        
+        else:
+            url = 'http://etsiit.ugr.es/pages/calendario_academico/horarios1516/' + descriptor
+            cj = cookielib.CookieJar()
+            
+            opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+            
+            request = urllib2.Request(url)
+            
+            f = opener.open(request)
+            data = f.read()
+            f.close()
+            opener.close()
+            
+            FILE = open(nombre_fichero, "wb+")
+            FILE.write(data)
+            FILE.close()
+            
+            bot.send_document(m.chat.id, open( nombre_fichero, 'rb'))
+            
+    except Exception as e:
+        bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
+
+
+        
+def obtener_horario(grado,m):
     if grado == 'informatica':
-        descriptor += 'i1516/!'
-        nombre_fichero += 'i.pdf'
+        comprobar_fichero('informatica',m)
         
     elif grado == 'teleco':
-        descriptor += 'tt1516/!'
-        nombre_fichero += 't.pdf'
-    
-    elif grado == 'matematicas':
-       descriptor +=  'm1516/!'
-       nombre_fichero += 'm.pdf'
-    
-    if os.path.isfile(nombre_fichero):
-        bot.send_document(id_chat, open( nombre_fichero, 'rb'))
-    
-    else:
-        url = 'http://etsiit.ugr.es/pages/calendario_academico/horarios1516/' + descriptor
-
-        bot.send_message(id_chat,url)
-        cj = cookielib.CookieJar()
-        
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-        
-        request = urllib2.Request(url)
-        
-        f = opener.open(request)
-        data = f.read()
-        f.close()
-        opener.close()
-        
-        FILE = open(nombre_fichero, "wb+")
-        FILE.write(data)
-        FILE.close()
-        
-        bot.send_document(id_chat, open( nombre_fichero, 'rb'))
-
-
-        
-def obtener_horario(grado,id_chat):
-    if grado == 'informatica':
-        comprobar_fichero('informatica',id_chat)
-        
-    elif grado == 'teleco':
-        comprobar_fichero('teleco',id_chat)
+        comprobar_fichero('teleco',m)
         
     elif grado == 'matematicas':
-        comprobar_fichero('matematicas',id_chat)
+        comprobar_fichero('matematicas',m)
         
 #Horario grado ingenieria informatica
     
 @bot.message_handler(commands=['horario_gii'])
 def obtener_horario_gii(m):
-    obtener_horario('informatica',m.chat.id)
+    obtener_horario('informatica',m)
 
 
 #Horario grado ingenieria telecomunicaciones
 
 @bot.message_handler(commands=['horario_git'])
 def obtener_horario_git(m):
-    obtener_horario('teleco',m.chat.id)
+    obtener_horario('teleco',m)
         
 #Horario grado ingenieria inf+mates
 
 @bot.message_handler(commands=['horario_gim'])
 def obtener_horario_gim(m):
-    obtener_horario('matematicas',m.chat.id)
+    obtener_horario('matematicas',m)
     
 while True: 
     time.sleep(300)
