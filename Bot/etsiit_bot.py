49216ca81352205061bad447a70fbb9662bf7e66
Alejandro Casado Quijada <alex22alex33@gmail.com>
2015-09-27 15:26:17 +0000

    Reestructuración del código

--- Bot/etsiit_bot.py
+++ Bot/etsiit_bot.py
@@ -18,111 +18,33 @@ from conf import token
 reload(sys) 
 sys.setdefaultencoding("utf-8")
 
-bot = telebot.TeleBot(token) # Creamos el objeto de nuestro bot.
+# Creamos el objeto bot que representa a ETSIITBOT
+bot = telebot.TeleBot(token)
 
-hideBoard = types.ReplyKeyboardHide()
-
-rateSelect = types.ReplyKeyboardMarkup(one_time_keyboard=True,resize_keyboard=True,selective=True)
-rateSelect.add('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado')
-
-
-def listener(messages):
-    for m in messages: 
-        ayuda_pasiva(m)
-        
-bot.set_update_listener(listener) 
-
-
-#Ayuda pasiva para obtener el horario y/o examenes
-def ayuda_pasiva(m):
-    
-    try:
-        
-        if hasattr(m, 'text'):
-            mensaje = ""
-                
-            #m.text[0] != "/" evita que la ayuda pasiva se active al user /horario_gii
-            if "horario" in m.text.lower() and m.text[0] != "/":
-                mensaje = open('../informacion/ayuda_pasiva_horario.txt', 'r').read()
-                bot.reply_to(m,mensaje)
-            
-            if ("examenes" in m.text.lower() or "exámenes" in m.text.lower()) and m.text[0] != "/" :
-                mensaje = open('../informacion/ayuda_pasiva_examenes.txt', 'r').read()
-                bot.reply_to(m,mensaje)
-        
-    except Exception as e:
-        exception_log(e,m)
-        
-# Funcion start
-
-@bot.message_handler(commands=['start'])
-def start(m):
-    
-    log(m)
-    
-    cid = m.chat.id
-    
-    try:
-        mensaje = open('../informacion/start.txt', 'r').read()
-        bot.send_message(cid,mensaje)
-        
-    except Exception as e:
-        exception_log(e,m)
-        bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
-        
-#Ayuda
-@bot.message_handler(commands=['ayuda'])
-def ayuda(m):
-    
-    log(m)
-    
-    cid = m.chat.id
-    
-    
-    try:
-        mensaje = open('../informacion/ayuda.txt', 'r').read()
-        bot.send_message(cid,mensaje)
-        
-    except Exception as e:
-        exception_log(e,m)
-        bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
-
-#Registro de actividad
-def log(m):
-    user_id = m.from_user.id
-    name = m.from_user.first_name
-    last_name = m.from_user.last_name
-    username = m.from_user.username
-    hour = time.strftime("%H:%M:%S")
-    date = time.strftime("%d/%m/%y")
-    information = "["+ str(date) + ' ' + str(hour) + ' ' +str(name)  + ' ' +  str(last_name) + ' ' + str(user_id) + ' @' + str(username) + "]: " + m.text 
-    
-    aux = open( '../informacion/registro.txt', 'a')
-    aux.write( str(information) + "\n")
-    aux.close()
- 
-def exception_log(e,m):
-    hour = time.strftime("%H:%M:%S")
-    date = time.strftime("%d/%m/%y")
-        
-    complete_date = 'Times exception: '  + str(date) + ' ' + str(hour) + '\n'
-    
-    cause = 'Cause of the exception: ' + str(m.text) + '\n'
-    
-    exception = 'Exception: ' + str(e)
-    
-    information = complete_date + cause + exception
-    
-    
-    aux = open( '../informacion/exception.txt', 'a') 
-    aux.write( str(information) + "\n\n")
-    aux.close()       
-    
-    
+# Para que el bot siga ejecutandose aunque presente errores
 bot.polling(none_stop=True) 
 
-#Funcion encarga de enviar el archivo al chat del mensaje
 
+####################################
+# Definimos los distintos teclados #
+####################################
+
+# Teclado para elegir el menu
+teclado_menu = types.ReplyKeyboardMarkup(one_time_keyboard=True,resize_keyboard=True,selective=True)
+teclado_menu.add('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado')
+
+# Teclado para elegir el horario
+teclado_horario = types.ReplyKeyboardMarkup(one_time_keyboard=True,resize_keyboard=True,selective=True)
+teclado_horario.add('Informática', 'Telecomunicaciones', 'Informática + matemáticas')
+
+# Ocultamos el teclado
+hideBoard = types.ReplyKeyboardHide()
+
+########################
+# Funciones auxiliares #
+########################
+
+# Función encarga de enviar el archivo al chat del mensaje
 def enviar_archivo(m,archivo):
     
     fo = open(archivo, 'rb')
@@ -131,7 +53,94 @@ def enviar_archivo(m,archivo):
 
     bot.send_document(m.chat.id, fo)
 
-    fo.close()    
+    fo.close()
+    
+# Limpiamos la fecha 
+def limpiar_fecha(aux):
+    
+    fecha_limpia = ""
+    
+    fecha_limpia = re.sub('<div class="numero">', '',aux)
+    
+    fecha_limpia = re.sub('</div>', '',fecha_limpia)
+    
+    fecha_limpia = re.sub('\s+',' ',fecha_limpia)
+    
+    return fecha_limpia
+    
+# Limpiamos plato   
+def limpiar_plato(aux):
+    
+    plato_limpio = ""
+    plato_limpio = re.sub('<br/>', ' ',aux)
+    plato_limpio = re.sub('\s+',' ',plato_limpio)
+    
+    return plato_limpio
+    
+    
+
+# Devuelve la informacion sobre el menú en una lista
+def obtener_menu():
+    
+    r  = requests.get("http://comedoresugr.tcomunica.org/")
+    
+    data = r.text
+    
+    soup = BeautifulSoup(data,"lxml")
+    
+    result = soup.find_all('div', id='plato')
+    
+    menu_semana = []
+    
+    for x in result:
+        menu_semana.append(str(x))
+
+    return menu_semana
+
+# Devuelve el menu de un dia concreto
+def obtener_menu_dia(dia):
+    
+    mensaje = ""
+    
+    #Obtenemos la fecha
+    fecha = dia.find('div', id='fechaplato')     # TODA LA INFORMACION SOBRE EL DIA DEL PLATO
+    
+    fecha = ''.join(map(str, fecha.contents))
+    
+    fecha = limpiar_fecha(fecha)
+    
+    mensaje += fecha + "\n\n"
+    
+
+    #Obtenemos el primer plato
+    plato1 = dia.find('div', id='plato1')
+    
+    plato1 = ''.join(map(str, plato1.contents))
+
+    plato1 = limpiar_plato(plato1)
+    
+    mensaje += plato1 + "\n"
+
+    #Obtenemos el segundo plato
+    plato2 = dia.find('div', id='plato2')
+    
+    plato2 = ''.join(map(str, plato2.contents))
+
+    plato2 = limpiar_plato(plato2)
+    
+    mensaje += plato2 + "\n"
+
+
+    #Obtenemos el tercer plato
+    plato3 = dia.find('div', id='plato3')
+    
+    plato3 = ''.join(map(str, plato3.contents))
+
+    plato3 = limpiar_plato(plato3)
+    
+    mensaje += plato3 + "\n\n"    
+
+    return mensaje
 
 # Comprobamos si existe el horario y se actua en consecuencia
 def mandar_horario(grado,m):
@@ -210,9 +219,143 @@ def mandar_examenes(grado,m):
     except Exception as e:
         bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
         exception_log(e,m)
+        
+#Función auxiliar usado por menu_dia
+def aux_menu_dia(m):
+    
+    try:
+        
+        dias = { 'Lunes': 0, 'Martes': 1, 'Miercoles': 2,'Jueves': 3,'Viernes': 4,'Sabado': 5}
+        
+        dia_seleccionado = str(m.text)
+        
+        mensaje = "¡Hola!\n\nAquí tienes el menú del " + dia_seleccionado.lower() + ", ¡Buen provecho! \n\n"
+        
+        info_dias = obtener_menu()
+        
+        aux = BeautifulSoup(info_dias[dias[dia_seleccionado]],"lxml")
 
-# Contacto
+        mensaje += obtener_menu_dia(aux)
+                
+        mensaje += "Precio por menú: 3,5€"
 
+        bot.reply_to(m, mensaje, reply_markup=hideBoard)    
+
+    except Exception as e:
+        bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
+        exception_log(e,m)
+        
+############
+# Listener #
+############
+
+def listener(messages):
+    for m in messages: 
+        ayuda_pasiva(m)
+        
+bot.set_update_listener(listener) 
+
+
+#####################
+# Registros del bot #
+#####################
+
+# Registro de actividad
+def log(m):
+    user_id = m.from_user.id
+    name = m.from_user.first_name
+    last_name = m.from_user.last_name
+    username = m.from_user.username
+    hour = time.strftime("%H:%M:%S")
+    date = time.strftime("%d/%m/%y")
+    information = "["+ str(date) + ' ' + str(hour) + ' ' +str(name)  + ' ' +  str(last_name) + ' ' + str(user_id) + ' @' + str(username) + "]: " + m.text 
+    
+    aux = open( '../informacion/registro.txt', 'a')
+    aux.write( str(information) + "\n")
+    aux.close()
+ 
+# Registro de excepciones
+def exception_log(e,m):
+    hour = time.strftime("%H:%M:%S")
+    date = time.strftime("%d/%m/%y")
+        
+    complete_date = 'Times exception: '  + str(date) + ' ' + str(hour) + '\n'
+    
+    cause = 'Cause of the exception: ' + str(m.text) + '\n'
+    
+    exception = 'Exception: ' + str(e)
+    
+    information = complete_date + cause + exception
+    
+    
+    aux = open( '../informacion/exception.txt', 'a') 
+    aux.write( str(information) + "\n\n")
+    aux.close()   
+
+########################
+# Ayuda pasiva del bot #
+########################
+
+#Ayuda pasiva para obtener el horario y/o examenes
+def ayuda_pasiva(m):
+    
+    try:
+        
+        if hasattr(m, 'text'):
+            mensaje = ""
+                
+            #m.text[0] != "/" evita que la ayuda pasiva se active al user /horario_gii
+            if "horario" in m.text.lower() and m.text[0] != "/":
+                mensaje = open('../informacion/ayuda_pasiva_horario.txt', 'r').read()
+                bot.reply_to(m,mensaje)
+            
+            if ("examenes" in m.text.lower() or "exámenes" in m.text.lower()) and m.text[0] != "/" :
+                mensaje = open('../informacion/ayuda_pasiva_examenes.txt', 'r').read()
+                bot.reply_to(m,mensaje)
+        
+    except Exception as e:
+        exception_log(e,m)
+        
+        
+#######################################
+# Comandos a los que reacciona el bot #
+#######################################
+
+# Función start
+@bot.message_handler(commands=['start'])
+def start(m):
+    
+    log(m)
+    
+    cid = m.chat.id
+    
+    try:
+        mensaje = open('../informacion/start.txt', 'r').read()
+        bot.send_message(cid,mensaje)
+        
+    except Exception as e:
+        exception_log(e,m)
+        bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
+        
+# Muestra la ayuda
+@bot.message_handler(commands=['ayuda'])
+def ayuda(m):
+    
+    log(m)
+    
+    cid = m.chat.id
+    
+    
+    try:
+        mensaje = open('../informacion/ayuda.txt', 'r').read()
+        bot.send_message(cid,mensaje)
+        
+    except Exception as e:
+        exception_log(e,m)
+        bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
+
+
+# Información de contacto de la escuela
 @bot.message_handler(commands=['contacto'])
 def contacto(m):
     
@@ -231,33 +374,28 @@ def contacto(m):
 
 
 #Examenes grado ingenieria informatica
-    
 @bot.message_handler(commands=['examenes_gii'])
 def obtener_examenes_gii(m):
     mandar_examenes('informatica',m)
     
 #Examenes grado ingenieria informatica
-    
 @bot.message_handler(commands=['examenes_gitt'])
 def obtener_examenes_gitt(m):
     mandar_examenes('teleco',m)
     
     
 #Horario grado ingenieria informatica
-    
 @bot.message_handler(commands=['horario_gii'])
 def obtener_horario_gii(m):
     mandar_horario('informatica',m)
 
 
 #Horario grado ingenieria telecomunicaciones
-
 @bot.message_handler(commands=['horario_gitt'])
 def obtener_horario_gitt(m):
     mandar_horario('teleco',m)
         
 #Horario grado ingenieria inf+mates
-
 @bot.message_handler(commands=['horario_gim'])
 def obtener_horario_gim(m):
     mandar_horario('matematicas',m)
@@ -327,96 +465,7 @@ def obtener_web(m):
         bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
         exception_log(e,m)
         
-
-
-   
-#Limpiamos la fecha 
-def limpiar_fecha(aux):
-    
-    fecha_limpia = ""
-    
-    fecha_limpia = re.sub('<div class="numero">', '',aux)
-    
-    fecha_limpia = re.sub('</div>', '',fecha_limpia)
-    
-    fecha_limpia = re.sub('\s+',' ',fecha_limpia)
-    
-    return fecha_limpia
-    
-#Limpiamos plato   
-def limpiar_plato(aux):
-    
-    plato_limpio = ""
-    plato_limpio = re.sub('<br/>', ' ',aux)
-    plato_limpio = re.sub('\s+',' ',plato_limpio)
-    
-    return plato_limpio
-    
-    
-
-#Devuelve el menú en una lista
-def obtener_menu():
-    
-    r  = requests.get("http://comedoresugr.tcomunica.org/")
-    
-    data = r.text
-    
-    soup = BeautifulSoup(data,"lxml")
-    
-    result = soup.find_all('div', id='plato')
-    
-    menu_semana = []
-    
-    for x in result:
-        menu_semana.append(str(x))
-
-    return menu_semana
-
-#Devuelve el menu de un dia concreto
-def obtener_menu_dia(dia):
-    
-    mensaje = ""
-    
-    #Obtenemos la fecha
-    fecha = dia.find('div', id='fechaplato')     # TODA LA INFORMACION SOBRE EL DIA DEL PLATO
-    
-    fecha = ''.join(map(str, fecha.contents))
-    
-    fecha = limpiar_fecha(fecha)
-    
-    mensaje += fecha + "\n\n"
-    
-
-    #Obtenemos el primer plato
-    plato1 = dia.find('div', id='plato1')
-    
-    plato1 = ''.join(map(str, plato1.contents))
-
-    plato1 = limpiar_plato(plato1)
-    
-    mensaje += plato1 + "\n"
-
-    #Obtenemos el segundo plato
-    plato2 = dia.find('div', id='plato2')
-    
-    plato2 = ''.join(map(str, plato2.contents))
-
-    plato2 = limpiar_plato(plato2)
-    
-    mensaje += plato2 + "\n"
-
-
-    #Obtenemos el tercer plato
-    plato3 = dia.find('div', id='plato3')
-    
-    plato3 = ''.join(map(str, plato3.contents))
-
-    plato3 = limpiar_plato(plato3)
-    
-    mensaje += plato3 + "\n\n"    
-
-    return mensaje
-    
+        
 #Obtiene y envia menú
 @bot.message_handler(commands=['menu_semana'])
 def menu_semana(m):
@@ -458,40 +507,15 @@ def menu_dia(m):
         
         texto = "Selecciona dia para saber el menú"
         
-        msg = bot.send_message(cid,texto, reply_markup=rateSelect)
+        msg = bot.send_message(cid,texto, reply_markup=teclado_menu)
         
         bot.register_next_step_handler(msg, aux_menu_dia)
         
     except Exception as e:
         bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
         exception_log(e,m)
-
-def aux_menu_dia(m):
-    
-    try:
         
         
-        dias = { 'Lunes': 0, 'Martes': 1, 'Miercoles': 2,'Jueves': 3,'Viernes': 4,'Sabado': 5}
-        
-        dia_seleccionado = str(m.text)
-        
-        mensaje = "¡Hola!\n\nAquí tienes el menú del " + dia_seleccionado.lower() + ", ¡Buen provecho! \n\n"
-        
-        info_dias = obtener_menu()
-        
-        aux = BeautifulSoup(info_dias[dias[dia_seleccionado]],"lxml")
-
-        mensaje = obtener_menu_dia(aux)
-                
-        mensaje += "Precio por menú: 3,5€"
-
-        bot.reply_to(m, mensaje, reply_markup=hideBoard)    
-
-    except Exception as e:
-        bot.reply_to(m,'Se ha producido un error, intentelo mas tarde')
-        exception_log(e,m)
-    
-    
-    
+# Evita sobrecarga de la cpu    
 while True: 
     time.sleep(300)
