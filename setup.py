4ea290d07212b887cdedef7718c171b5a2739c19
Alejandro Casado Quijada <alex22alex33@gmail.com>
2015-10-29 16:56:55 +0000

    Correción setup

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 setup(name='ETSIITBOT',
 	version='0.8',
 	description='Bot de telegram sobre la ETSIIT-UGR',
-	url='https://github.com/acasadoquijada/IV',
+	url='https://github.com/acasadoquijada/ETSIIT_BOT',
 	author='Alejandro Casado Quijada',
 	author_email='acasadoquijada@gmail.com',
 	license='GNU GPL',
-	#packages=['ETSIITBOT'],
 	install_requires=['beautifulsoup4','py','pyTelegramBotAPI','pytest','requests','six','wheel'],
 	zip_safe=False)
