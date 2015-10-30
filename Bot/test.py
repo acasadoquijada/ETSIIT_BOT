ec188ff5a2a0d29d5106f5875182877f25a48286
Alejandro Casado Quijada <alex22alex33@gmail.com>
2015-10-30 16:59:16 +0000

    Test

--- Bot/test.py
+++ Bot/test.py
@@ -10,12 +10,6 @@ should_skip = 'TOKEN' and 'CHAT_ID' not in os.environ
 
 class TestTeleBot():
 
-    def test_message_listener(self):
-        msg_list = []
-        for x in range(100):
-            msg_list.append(self.create_text_message('Message ' + str(x)))
-            
-            
     def test_localizacion(self):
       
         tb = telebot.TeleBot(TOKEN)
