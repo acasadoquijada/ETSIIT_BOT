e2c402376e671d6cb1aaebcd33f2c551bf3f8435
Alejandro Casado Quijada <alex22alex33@gmail.com>
2015-10-30 16:53:38 +0000

    Test

--- Bot/test.py
+++ Bot/test.py
@@ -14,3 +14,13 @@ class TestTeleBot():
         msg_list = []
         for x in range(100):
             msg_list.append(self.create_text_message('Message ' + str(x)))
+            
+            
+    def test_localizacion(self):
+      
+        tb = telebot.TeleBot(TOKEN)
+        lat = 26.3875591
+        lon = -161.2901042
+        ret_msg = tb.send_location(CHAT_ID, lat, lon)
+        assert int(ret_msg.location.longitude) == int(lon)
+        assert int(ret_msg.location.latitude) == int(lat)
